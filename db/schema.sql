c79c2fc67932f600c09de619ae50d56bfca2f26f
Kahlium <kahligtz@gmail.com>
2024-06-17 22:12:49 -0600

    database tables created

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,24 @@ CREATE DATABASE business_db;
 
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    manager_id INTEGER,
+    role_id INTEGER NOT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    manager_id INTEGER
+    REFERENCES roles(id)
 );
--- server.js
+++ server.js
@@ -18,3 +18,29 @@ const pool = new Pool(
 )
 
 pool.connect();
+
+app.get('/api/employee', (req, res) => {
+    const sql = `SELECT id, first_name FROM employee`;
+  
+    pool.query(sql, (err, { rows }) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
